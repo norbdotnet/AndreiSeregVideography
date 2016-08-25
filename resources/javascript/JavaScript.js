7f012d43f82d88e00efd6efc2f7fd9b76b6fbe50
norbdotnet <norbdotnet@gmail.com>
2016-08-25 02:35:59 -0400

    added images #1

--- resources/javascript/JavaScript.js
+++ resources/javascript/JavaScript.js
@@ -43,7 +43,7 @@ $('document').ready(function () {
 //        position: map.getCenter(),
 //        map: map,
 //        optimized: false,
-//        icon: '/assets/images/mapMarker.png'
+//        icon: '/resources/images/mapMarker.png'
 //    });
 //}
 
--- resources/javascript/yt.js
+++ resources/javascript/yt.js
@@ -1,4 +1,4 @@
-﻿"use strict";
+"use strict";
 
 var player;
 var playPauseButtons = [];
